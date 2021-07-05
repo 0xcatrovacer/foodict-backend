b47ad08f56b529f4d319913fa36499e744a16d2b
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-05 11:25:43 +0530

    added method to not return hashed password and tokens

--- src/models/user.js
+++ src/models/user.js
@@ -32,15 +32,15 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-// userSchema.methods.toJSON = function () {
-//     const user = this
-//     const userObject = user.toObject()
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
 
-//     delete userObject.password
-//     delete userObject.tokens
+    delete userObject.password
+    delete userObject.tokens
 
-//     return userObject
-// }
+    return userObject
+}
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
