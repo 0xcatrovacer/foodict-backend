2458cc003ad102045419093c0e0b3c1dc12d5054
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-10 20:13:37 +0530

    added basic order model

---
+++ src/models/order.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    order: [
+        {
+            type: Object,
+        },
+    ],
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
