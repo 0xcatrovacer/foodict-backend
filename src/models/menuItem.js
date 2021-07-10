bd83296bea7e1a3fdbb6330f7e8dd187b7c6d7b8
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-10 12:09:20 +0530

    added model menuItem

---
+++ src/models/menuItem.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const menuItemSchema = new mongoose.Schema({
+  item_type: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  item_name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  item_search_term: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  veg_type: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  cuisine_type: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  price: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+});
+
+const MenuItem = mongoose.model("MenuItem", menuItemSchema);
+
+module.exports = MenuItem;
