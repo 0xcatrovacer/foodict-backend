104212b0bccf68e9d51b80b2e1ff9399159b6468
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-10 11:41:40 +0530

    added router to enter eatery details

---
+++ src/routers/eatery-routers.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const Eatery = require("../models/eatery");
+
+const router = new express.Router();
+
+//Enter Eatery Details
+router.post("/eatery/create", async (req, res) => {
+  const eatery = new Eatery(req.body);
+  try {
+    eatery.save();
+    res.status(201).send(eatery);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
