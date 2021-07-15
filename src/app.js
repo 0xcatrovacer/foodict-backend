2acc547408b211760b9f4091a9e28e1375185228
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-16 02:23:32 +0530

    handled CORS error

--- src/app.js
+++ src/app.js
@@ -9,6 +9,17 @@ require("./db/mongoose");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Headers", "*");
+    res.set("Access-Control-Allow-Methods", "*");
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
+    }
+    next();
+});
+
 app.use(express.json());
 
 app.use(userRouter);
