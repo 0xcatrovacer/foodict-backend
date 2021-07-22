8eebeb0bedd0e2f227a6aaad0de8898aac5ab62c
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-22 22:33:46 +0530

    added timestamps in order module

--- src/models/order.js
+++ src/models/order.js
@@ -1,21 +1,26 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-    order: [
-        {
-            type: Object,
+const orderSchema = new mongoose.Schema(
+    {
+        order: [
+            {
+                type: Object,
+            },
+        ],
+        orderPrice: {
+            type: Number,
+            required: true,
         },
-    ],
-    orderPrice: {
-        type: Number,
-        required: true,
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
     },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 
