302803211e56bbac241c9f16c77ad415f1cbb2f4
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-22 20:23:56 +0530

    integrated razorpay

--- src/routers/payment-routers.js
+++ src/routers/payment-routers.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const Razorpay = require("razorpay");
+const shortid = require("shortid");
 const auth = require("../middlewares/auth");
+
+const instance = new Razorpay({
+    key_id: process.env.FOODICT_RZPAY_KEYID,
+    key_secret: process.env.FOODICT_RZPAY_KEYSECRET,
+});
 
 const router = new express.Router();
 
 //Create an Order
-router.post("/payments/order", auth, (req, res) => {
-    const instance = new Razorpay({
-        key_id: process.env.FOODICT_RZPAY_KEYID,
-        key_secret: process.env.FOODICT_RZPAY_KEYSECRET,
-    });
-
+router.post("/payments/order", auth, async (req, res) => {
     const options = {
-        amount: "20000",
+        amount: 20000,
         currency: "INR",
+        receipt: shortid.generate(),
+        payment_capture: 1,
     };
-    instance.orders.create(options, function (err, order) {
-        res.send(order);
-    });
+    try {
+        const response = await instance.orders.create(options);
+        res.send({
+            id: response.id,
+            currency: response.currency,
+            amount: response.amount,
+        });
+    } catch (e) {
+        res.send(e);
+    }
 });
 
 module.exports = router;
